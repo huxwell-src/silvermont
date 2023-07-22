41821bf8a99131d4d4ee58fd5fa0605dadba5de4
huxwell-src <65736835+huxwell-src@users.noreply.github.com>
2023-07-22 15:40:12 -0400

    4 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { } from 'react'
-import Hero from './Components/Hero'
-import './App.css'
+import {} from 'react';
+import Hero from './Components/Hero';
+import './App.css';
 
 function App() {
   return (
-    <> 
+    <>
       <Hero></Hero>
+      <div className="bg-sky-500 h-[100dvh] w-full "></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,32 +1,45 @@
-import {} from 'react'
-import {Logo} from './Icons'
+import {} from 'react';
+import { Logo, Menu } from './Icons';
 
 const Navbar = () => {
-    const links = [
-        { id: 1, title: 'Inicio', url: '#' },
-        { id: 2, title: 'Servicios', url: '#' },
-        { id: 3, title: 'Acerca de', url: '#' },
-        { id: 4, title: 'Contacto', url: '#' },
-      ];
+  const links = [
+    { id: 1, title: 'Inicio', url: '#' },
+    { id: 2, title: 'Servicios', url: '#' },
+    { id: 3, title: 'Acerca de', url: '#' },
+    { id: 4, title: 'Contacto', url: '#' },
+  ];
+
+  const Width = () => 125;
+
   return (
-    <header className=' z-[999] absolute top-0 left-0 w-full flex justify-between items-center py-4 px-10 md:px-20 lg:px-52 duration-75' >
-    <a href="#">   <Logo color="#252525" width={170} /> </a>
-    <div className='relative ' >
-        <div className='' >
-            {links.map((link) => (
-            <a key={link.id} href={link.url} 
-            className='
+    <header className="z-[999] absolute px-10 py-4 flex justify-between items-center duration-75">
+      <a href="#">
+        {' '}
+        <Logo color="#252525" width={Width()} />
+      </a>
+      <div className="relative ">
+        <div className="sm:hidden">
+          <Menu color="#FDFDFD" width={7} />
+        </div>
+        <div className="">
+          {links.map((link) => (
+            <a
+              key={link.id}
+              href={link.url}
+              className=' hidden sm:inline
             relative text-light-black text-lg font-medium decoration-none ml-4 
             transition-all duration-200 ease-in-out 
             before:absolute before:content-[" "] before:bg-gold before:w-0 before:h-[3px] before:rounded-full before:bottom-0 before:left-0 
             before:transition-all before:duration-200 before:ease-in-out 
-            hover:text-gold hover:before:w-full'>
-                {link.title}
-            </a>))}
+            hover:text-gold hover:before:w-full'
+            >
+              {link.title}
+            </a>
+          ))}
         </div>
-    </div>
-</header>
-  )
-}
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
